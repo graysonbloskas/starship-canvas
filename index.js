334d1c3ae1e599921c66ad88043f96a1f6c6af61
Grayson Bloskas <graysonbloskas@Graysons-iMac-Pro.local>
2021-09-01 16:57:15 -0400

    created projectile to be shot out of the player, set up angles and velocity and click event to recognize where the user clicks

--- index.js
+++ index.js
@@ -37,6 +37,12 @@ class Projectile {
     ctx.fillStyle = this.color;
     ctx.fill();
   }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
 }
 
 const x = canvas.width / 2;
@@ -45,14 +51,27 @@ const y = canvas.height / 2;
 const player = new Player(x, y, 30, 'blue');
 player.draw();
 
-addEventListener('click', (event) => {
-  const projectile = new Projectile(
-    event.clientX,
-    event.clientY,
-    5,
-    'red',
-    null
-  );
+const projectiles = [];
 
-  projectile.draw();
+function animate() {
+  requestAnimationFrame(animate);
+  projectiles.forEach((projectile) => {
+    projectile.update();
+  });
+}
+
+addEventListener('click', (event) => {
+  const angle = Math.atan2(
+    event.clientY - canvas.height / 2,
+    event.clientX - canvas.width / 2
+  );
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  };
+  projectiles.push(
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
+  );
 });
+
+animate();

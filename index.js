133da80fec112623d7f96b062331931aeec6a677
Grayson Bloskas <graysonbloskas@Graysons-iMac-Pro.local>
2021-09-01 15:08:20 -0400

    initial file upload, canvas player set up, documents connected.

---
+++ index.js
@@ -0,0 +1,24 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+}
+
+const player = new Player(100, 100, 30, 'blue');
+player.draw();

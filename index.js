fb4e71e23247e3b749cb31ac21f8dd76bd66dc45
Grayson Bloskas <graysonbloskas@Graysons-iMac-Pro.local>
2021-09-01 15:27:23 -0400

    created projectile that will shoot out of player

--- index.js
+++ index.js
@@ -14,11 +14,45 @@ class Player {
 
   draw() {
     ctx.beginPath();
+    // ctx.moveTo(65, 50);
+    // ctx.lineTo(100, 75);
+    // ctx.lineTo(100, 25);
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     ctx.fillStyle = this.color;
     ctx.fill();
   }
 }
 
-const player = new Player(100, 100, 30, 'blue');
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+}
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const player = new Player(x, y, 30, 'blue');
 player.draw();
+
+addEventListener('click', (event) => {
+  const projectile = new Projectile(
+    event.clientX,
+    event.clientY,
+    5,
+    'red',
+    null
+  );
+
+  projectile.draw();
+});

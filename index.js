1731252413a570ea85380ed68114317eb914c375
Grayson Bloskas <graysonbloskas@Graysons-iMac-Pro.local>
2021-09-02 16:31:51 -0400

    created particle explosions, decreased the size of enemies on hit, added some other cool effects like particle friction, etc.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+console.log(gsap);
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-
 
 // Player Class
 class Player {
@@ -68,12 +68,45 @@ class Enemy {
   }
 }
 
+// adding friction to particles to slow down particle speed and distnace after impact
+const friction = 0.99
+// Particle class
+class Particle {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.alpha = 1;
+  }
+  draw() {
+    ctx.save();
+    ctx.globalAlpha = this.alpha
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.restore()
+  }
+
+  update() {
+    this.draw();
+    this.velocity.x *= friction;
+    this.velocity.y *= friction;
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+    this.alpha -= 0.01
+  }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 15, 'white');
 const projectiles = [];
 const enemies = [];
+const particles = [];
 
 function spawnEnemies() {
   setInterval(() => {
@@ -106,28 +139,36 @@ let animationId;
 function animate() {
   animationId = requestAnimationFrame(animate);
   // Setting background space image
-//   var background = new Image();
-//   background.src = "https://i.redd.it/imkv74m4q5g41.png";
-//   background.onload = function() {
-//       var pattern = ctx.createPattern(background, 'repeat') 
-//       ctx.fillStyle = pattern
-//       ctx.fillRect(0, 0, canvas.width, canvas.height);
-//     }
-    
-    // Black background with cool faded animation of objects
-ctx.fillStyle = 'rgba(0, 0, 0, 0.1)'
-ctx.fillRect(0, 0, canvas.width, canvas.height)
+  //   var background = new Image();
+  //   background.src = "https://i.redd.it/imkv74m4q5g41.png";
+  //   background.onload = function() {
+  //       var pattern = ctx.createPattern(background, 'repeat')
+  //       ctx.fillStyle = pattern
+  //       ctx.fillRect(0, 0, canvas.width, canvas.height);
+  //     }
 
+  // Black background with cool faded animation of objects
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   player.draw();
+  particles.forEach((particle, index) => {
+    if (particle.alpha <= 0) {
+      particles.splice(index, 1);
+    } else {
+      particle.update();
+    }
+    
+  });
   projectiles.forEach((projectile, index) => {
     projectile.update();
     // Removes projectiles from being animated after they have left the canvas
-    if (projectile.x + projectile.radius < 0 || 
-        projectile.x - projectile.radius > canvas.width ||
-        projectile.y + projectile.radius < 0 ||
-        projectile.y - projectile.radius > canvas.height
-        ) {
+    if (
+      projectile.x + projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
       setTimeout(() => {
         projectiles.splice(index, 1);
       }, 0);
@@ -147,19 +188,40 @@ ctx.fillRect(0, 0, canvas.width, canvas.height)
     projectiles.forEach((projectile, projectileIndex) => {
       // distance detection between projectile and enemy
       const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
-
+      // When projectiles touch enemy
       if (dist - enemy.radius - projectile.radius < 1) {
-        setTimeout(() => {
-          enemies.splice(index, 1);
-          projectiles.splice(projectileIndex, 1);
-        }, 0);
+
+
+        // Particles - create explosions on collision with enemy
+        for (let i = 0; i < enemy.radius * 2; i++) {
+          particles.push(
+            new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.color, {
+              x: (Math.random() - 0.5) * (Math.random() * 8),
+              y: (Math.random() - 0.5) * (Math.random() * 8),
+            })
+          );
+        }
+
+        if (enemy.radius - 10 > 7) {
+          gsap.to(enemy, {
+            radius: enemy.radius - 10,
+          });
+          setTimeout(() => {
+            projectiles.splice(projectileIndex, 1);
+          }, 0);
+        } else {
+          setTimeout(() => {
+            enemies.splice(index, 1);
+            projectiles.splice(projectileIndex, 1);
+          }, 0);
+        }
       }
     });
   });
 }
 
 addEventListener('click', (event) => {
-    console.log(projectiles)
+  console.log(projectiles);
   const angle = Math.atan2(
     event.clientY - canvas.height / 2,
     event.clientX - canvas.width / 2
